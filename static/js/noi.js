6ecd9fa7a39c83e4b0588c79489bfd1e94685b3c
daniel <bro.danielx@gmail.com>
2017-09-26 23:12:20 -0400

    mobile fixes

--- static/js/noi.js
+++ static/js/noi.js
@@ -57,7 +57,7 @@ sundayma.addEventListener('click', function() {
 //   }
 // });
 
-$('a[href*="#"]:not([href="#"])').on('click touchstart', function() {
+$('a[href*="#"]:not([href="#"])').on('click tap', function() {
   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
