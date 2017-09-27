25546af42dd4612b9a2fd4b130337fa23cc90d81
daniel <bro.danielx@gmail.com>
2017-09-26 20:33:32 -0400

    mobile layout

--- static/js/noi.js
+++ static/js/noi.js
@@ -44,7 +44,20 @@ sundayma.addEventListener('click', function() {
   sundaymeeting.classList.toggle('collapse');
 });
 
-$('a[href*="#"]:not([href="#"])').click(function() {
+// $('a[href*="#"]:not([href="#"])').click(function() {
+//   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//     var target = $(this.hash);
+//     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//     if (target.length) {
+//       $('html, body').animate({
+//         scrollTop: target.offset().top
+//       }, 900);
+//       return false;
+//     }
+//   }
+// });
+
+$('a[href*="#"]:not([href="#"])').on('click touchstart', function() {
   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
